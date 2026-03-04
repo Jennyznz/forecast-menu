9cb6afc58eac1c92c9e9004aef3d81cdaa27faf1
Jenny <156132099+Jennyznz@users.noreply.github.com>
2026-03-04 10:03:21 -0500

    Update color scheme

--- src/recipes.js
+++ src/recipes.js
@@ -34,34 +34,94 @@ async function displayRecipes() {
         // Select random recipes from the results returned by Spoonacular
         const [randB, randL, randD] = [getRandRecipe(bRecipe), getRandRecipe(lRecipe), getRandRecipe(dRecipe)];
         
-        const breakfastTitle = randB?.title || "No recipe found";
-        const lunchTitle = randL?.title || "No recipe found";
-        const dinnerTitle = randD?.title || "No recipe found";
+        // const breakfastTitle = randB?.title || "No recipe found";
+        // const lunchTitle = randL?.title || "No recipe found";
+        // const dinnerTitle = randD?.title || "No recipe found";
 
         // Create recipe objects for each meal
-        const breakfast = new Recipe(breakfastTitle); // Search Recipe Info for more info later on?
-        const lunch = new Recipe(lunchTitle);
-        const dinner = new Recipe(dinnerTitle);
+        const breakfast = new Recipe(randB);
+        const lunch = new Recipe(randL);
+        const dinner = new Recipe(randD);
 
-        // Display Recipe object. 
+        // Display Recipe object and add Event Listeners
         const bMealBox = document.createElement('div'); // Have it sort of float over the second chunk?
+        bMealBox.recipeData = breakfast;
         bMealBox.innerHTML = breakfast.title;
         bMealBox.classList.add('meal-box');
+        mealListener(bMealBox);
         chunks[bIndex].innerHTML = ''; // Clear before adding (User may generate recipes multiple times in a row)
         chunks[bIndex].append(bMealBox);
 
         const lMealBox = document.createElement('div'); 
+        lMealBox.recipeData = lunch;
         lMealBox.innerHTML = lunch.title;
         lMealBox.classList.add('meal-box');
+        mealListener(lMealBox);
         chunks[lIndex].innerHTML = '';  // Clear before adding
         chunks[lIndex].append(lMealBox);
 
         const dMealBox = document.createElement('div'); 
+        dMealBox.recipeData = dinner;
         dMealBox.innerHTML = dinner.title;
         dMealBox.classList.add('meal-box');
+        mealListener(dMealBox);
         chunks[dIndex].innerHTML = '';  // Clear before adding
         chunks[dIndex].append(dMealBox);
     }
+}
+
+function mealListener(box) {
+    box.addEventListener('click', () => {
+        const recipeObject = box.recipeData;
+        const popup = displayPopup(recipeObject, box);
+        document.addEventListener('click', (e) => {
+            if (!popup.contains(e.target)) {
+                removePopup(box);
+            }
+        })
+
+    });
+}
+
+
+
+// OK, you can append popup to box.parent, aka the chunk, whose position is already relative! Then adjust the css so that the popup is to the right of the mealbox
+function displayPopup(recipe, box) {
+    // Create elements
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    const title = document.createElement('div');
+    const image = document.createElement('img');
+    const summary = document.createElement('div');
+    const ingLabel = document.createElement('h4');
+    const ing = document.createElement('ul');
+    const stepsLabel = document.createElement('h4');
+    const steps = document.createElement('ul');
+
+    // Fill elements with content
+    title.textContent = recipe.title;
+    image.src = recipe.image;
+    summary.innerHTML = recipe.summary || '';     // Spoonacular summaries usually contain HTML tags
+    ingLabel.textContent = 'Ingredients';
+    ing.innerHTML =
+        recipe.ingredients.map(i => `<li>${i}</li>`).join('') || ''; // What does .join do here?
+    stepsLabel.textContent = 'How To Cook';
+    steps.innerHTML =
+        recipe.steps.map(s => `<li>${s}</li>`).join('') || '';
+
+    // Append elements
+    popup.append(title, image, summary, ingLabel, ing, stepsLabel, steps);
+    const chunk = box.parentElement;
+    chunk.append(popup);
+
+    return popup;
+    // DISPLAY HEADER: X, VIEWFULL. CRUD
+}
+
+function removePopup(box) {
+    const chunk = box.parentElement;
+    const popup = chunk.querySelector('.popup');
+    popup.remove();
 }
 
 // Helper function to generateRecipes()
@@ -89,7 +149,11 @@ function buildRecipeQuery(category) {
         query: randProfile.query,
         type: randProfile.type || undefined, // May not exist
         number: 20,  
-        apiKey: 'd20a6c6b176842699146780873ea40c3', // Outdated API key
+        addRecipeInformation: true,
+        addRecipeNutrition: true,
+        fillIngredients: true,
+        instructionsRequired: true,
+        apiKey: '05713a7b3c03472f851322df4155af20', // Outdated API key
     }
 
     // URL pieces
@@ -115,11 +179,37 @@ function getRandRecipe(recipe) {
 }
 
 class Recipe {
-    constructor(title) {   
-        this.title = title;
+    constructor(fullData) {   
+        if (!fullData) {
+            this.id = null;
+            this.title = "No recipe found";
+            this.ingredients = [];
+            this.steps = [];
+            return;
+        } else {
+            this.id = fullData.id;
+            this.title = fullData.title || 'No recipes found';
+            this.image = fullData.image;
+            this.summary = fullData.summary;
+
+            this.servings = fullData.servings;
+            this.readyInMinutes = fullData.readyInMinutes;
+
+            // Store ingredients as strings
+            this.ingredients = fullData.extendedIngredients?.map(i => i.original) || [];
+
+            // Store important/basic information about available nutrients
+            this.nutrients = fullData.nutrition?.nutrients?.map(n => ({
+                name: n.name,
+                amount: n.amount,
+                unit: n.unit,
+                percent: n.percentOfDailyNeeds
+            })) || [];
+
+            this.steps = fullData.analyzedInstructions?.[0]?.steps.map(s => s.step) || [];
+        }
+
     }
 }
-
-
 
 export { displayRecipes };
